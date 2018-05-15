2c5c80a691674fe3ef6fbb0648efbb17a21cf3fe
mccomb <erik.k.mccomb@gmail.com>
2018-05-15 16:26:48 -0600

    corrected open times spacing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
             <Flexbox flexDirection='row'>
               <table style={styles.mailTable}>
                 <tr>
-                  <td rowSpan='3' style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'10px', }}>
+                  <td rowSpan='3' style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'5px', minWidth:'50%' }}>
                     <h3>
                       Mail Services in New Orleans
                     </h3>
@@ -26,7 +26,7 @@ class Home extends React.Component {
                       Doing business with Parcels and Post is easy. We are proud to be an integral part of the community serving our friends and neighbors. Come in for a relaxing shipping experience – our customers love us!
                     </p>
                   </td>
-                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'10px', width:'220px'}}>
+                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'5px', width:'200px'}}>
                     <Flexbox flexDirection='row' alignContent='center' justifyContent='center'>
                       <Flexbox justifyContent='center' alignItems='center'>
                         <div style={{padding:'0px 10px 0px 0px'}}>
@@ -45,7 +45,7 @@ class Home extends React.Component {
                   </td>
                 </tr>
                 <tr>
-                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'10px'}}>
+                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'5px'}}>
                     <Flexbox flexDirection='row' alignContent='center' justifyContent='center'>
                       <Flexbox alignContent='center' justifyContent='center'>
                         <div style={{padding:'0px 10px 0px 0px'}}>
@@ -61,65 +61,67 @@ class Home extends React.Component {
                   </td>
                 </tr>
                 <tr>
-                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'10px'}}>
+                  <td style={{backgroundColor: 'rgba(0,0,0,0.7)', padding:'5px',}}>
+                    <Flexbox alignContent='center' justifyContent='center'>
                     <table>
                       <tr>
-                        <td>
-                        Monday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Mon.
                         </td>
                         <td>
                         9:00 am - 6:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Tuesday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Tues.
                         </td>
                         <td>
                         9:00 am - 6:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Wednesday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Wed.
                         </td>
                         <td>
                         9:00 am - 6:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Thursday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Thur.
                         </td>
                         <td>
                         9:00 am - 6:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Friday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Fri.
                         </td>
                         <td>
                         9:00 am - 6:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Saturday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Sat.
                         </td>
                         <td>
                         9:00 am - 2:00 pm
                         </td>
                       </tr>
                       <tr>
-                        <td>
-                        Sunday
+                        <td style={{padding:'0px 15px 0px 0px'}}>
+                        Sun.
                         </td>
                         <td>
                         Closed
                         </td>
                       </tr>
                     </table>
+                    </Flexbox>
                   </td>
                 </tr>
                 <tr>
