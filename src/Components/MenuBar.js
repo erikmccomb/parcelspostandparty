91918165894f0111945613a1c4d80067a1ba0e17
mccomb <erik.k.mccomb@gmail.com>
2018-05-21 14:01:55 -0600

    added services dropdown

--- src/Components/MenuBar.js
+++ src/Components/MenuBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Image, Menu, Segment} from 'semantic-ui-react'
+import { Image, Menu, Segment, Dropdown } from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import Flexbox from 'flexbox-react'
 import pplogonotag from '../Images/Logos/pplogonotag.png'
@@ -17,31 +17,54 @@ class Menubar extends React.Component {
       <Flexbox flexDirection='column'>
         <Flexbox flexDirection='row' style={styles.barAlign}>
           <Menu pointing secondary>
+
+            <Dropdown text='Services' pointing className='link item' style={{fontFamily: 'Baskerville', alignItems: 'center', justifyContent: 'center', }}>
+              <Dropdown.Menu>
+
+                <Dropdown.Item>
+                  <Link to='Shipping' style={{color: 'black'}} >
+                    Shipping
+                  </Link>
+                </Dropdown.Item>
+
+                <Dropdown.Item>
+                  <Link to='Shipping' style={{color: 'black'}} >
+                    Shipping
+                  </Link>
+                </Dropdown.Item>
+                
+                <Dropdown.Item>
+                  <Link to='Shipping' style={{color: 'black'}} >
+                    Shipping
+                  </Link>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
             <Menu.Item name='FAQ' active={activeItem === 'FAQ'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', }}>
               <Link to='Faq' style={{color: 'black'}} >
                 FAQ
               </Link>
             </Menu.Item>
-            <Menu.Item name='Services' active={activeItem === 'Services'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', }}>
-              <Link to='Services' style={{color: 'black'}} >
-                Services
-              </Link>
-            </Menu.Item>
+
             <Menu.Item name='home' active={activeItem === 'home'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', padding:'6px'}}>
               <Link to='/' style={{color: 'black'}} >
                 <Image src={pplogonotag} style={{maxHeight: '35px'}} />
               </Link>
             </Menu.Item>
-            <Menu.Item name='Partners' active={activeItem === 'Partners'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', }}>
-              <Link to='Partners' style={{color: 'black'}} >
-                Partners
-              </Link> 
-            </Menu.Item>
+
             <Menu.Item name='About' active={activeItem === 'About'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', }}>
               <Link to='About' style={{color: 'black'}} >
                 About
               </Link>
             </Menu.Item>
+
+            <Menu.Item name='Partners' active={activeItem === 'Partners'} onClick={this.handleItemClick} style={{fontFamily: 'Baskerville', }}>
+              <Link to='Partners' style={{color: 'black'}} >
+                Partners
+              </Link> 
+            </Menu.Item>
+
           </Menu>
         </Flexbox>
       </Flexbox>
@@ -53,7 +76,6 @@ const styles = {
   barAlign: {
     width: '100vw',
     maxWidth: '100vw',
-    alignItems: 'center', 
     justifyContent: 'center',
     backgroundColor: '#FFFFFF',
     color: '#000000',
