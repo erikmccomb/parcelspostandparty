15d23d92f17309daa4cfb946fc09288b97bf8a59
mccomb <erik.k.mccomb@gmail.com>
2018-07-05 10:00:47 -0600

    corrected services

--- src/Desktop/DesktopSwitch.js
+++ src/Desktop/DesktopSwitch.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-
-// MenuBar Pages
-  import Home from './Pages/Home'
-  import Faq from './Pages/Faq'
-  import Partners from './Pages/Partners'
-  import About from './Pages/About'
-  
-// DropDown Pages
-  import Hotshot from './Pages/Services/Hotshot'
-  import Shipping from './Pages/Services/Shipping'
-  import Mailboxes from './Pages/Services/Mailboxes'
-  import Gallery from './Pages/Services/Gallery'
+import Home from './Pages/Home'
+import Faq from './Pages/Faq'
+import Partners from './Pages/Partners'
+import About from './Pages/About'
+import Services from './Pages/Services'
 
 const DesktopSwitch = () => (
   <main>
@@ -22,12 +15,7 @@ const DesktopSwitch = () => (
       <Route path='/FAQ' component={Faq} />
       <Route path='/Partners' component={Partners} />
       <Route path='/About' component={About} />
-
-    {/* Dropdown Selections */}
-      <Route path='/Shipping' component={Shipping} />
-      <Route path='/MailAndPackages' component={Mailboxes} />
-      <Route path='/Hotshot' component={Hotshot} />
-      <Route path='/Gallery' component={Gallery} />
+      <Route path='/Services' component={Services} />
     </Switch>
   </main>
 )
--- src/Mobile/MobileSwitch.js
+++ src/Mobile/MobileSwitch.js
@@ -1,33 +1,21 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-
-// MenuBar Pages
-  import Home from './Pages/Home'
-  import Faq from './Pages/Faq'
-  import Partners from './Pages/Partners'
-  import About from './Pages/About'
-  
-// DropDown Pages
-  import Hotshot from './Pages/Services/Hotshot'
-  import Shipping from './Pages/Services/Shipping'
-  import Mailboxes from './Pages/Services/Mailboxes'
-  import Gallery from './Pages/Services/Gallery'
+import Home from './Pages/Home'
+import Faq from './Pages/Faq'
+import Partners from './Pages/Partners'
+import About from './Pages/About'
+import Services from './Pages/Services'
 
 const MobileSwitch = () => (
   <main>
     <Switch>
 
     {/* MenuBar Selections */}
-      <Route exact path='/' component={Home} />
+    <Route exact path='/' component={Home} />
       <Route path='/FAQ' component={Faq} />
       <Route path='/Partners' component={Partners} />
       <Route path='/About' component={About} />
-
-    {/* Dropdown Selections */}
-      <Route path='/Shipping' component={Shipping} />
-      <Route path='/MailAndPackages' component={Mailboxes} />
-      <Route path='/Hotshot' component={Hotshot} />
-      <Route path='/Gallery' component={Gallery} />
+      <Route path='/Services' component={Services} />
     </Switch>
   </main>
 )
