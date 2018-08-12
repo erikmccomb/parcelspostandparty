0f64f9518ba46695d12e78b94090ab0a31e8e813
mccomb <erik.k.mccomb@gmail.com>
2018-08-12 14:18:27 -0600

    map API misbehaving

--- src/Desktop/Components/Map.js
+++ src/Desktop/Components/Map.js
@@ -12,10 +12,10 @@ const Map = compose(
     /**
      * Note: create and replace your own key in the Google console.
      * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyCsZMf5VXOxrO5XJy4SiHUP5KqBtKiULiI" can be ONLY used in this sandbox (no forked).
+     * The key "AIzaSyAbFo3j0y2p5AWljypOJI39FqSjSumWBY4" can be ONLY used in this sandbox (no forked).
      */
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyCsZMf5VXOxrO5XJy4SiHUP5KqBtKiULiI",
+      "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAbFo3j0y2p5AWljypOJI39FqSjSumWBY4",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px`, width: '590px', maxWidth: `100vw`,padding: '7px', backgroundColor:'rgba(0,0,0,0.6)',}} />,
     mapElement: <div style={{ height: `100%` }} />
