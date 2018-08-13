0996b5d11e05563dc188fb9f5ed822b36ec37def
mccomb <erik.k.mccomb@gmail.com>
2018-08-12 19:43:05 -0600

    mobile map API corrected

--- src/Mobile/Components/Map.js
+++ src/Mobile/Components/Map.js
@@ -12,10 +12,10 @@ const Map = compose(
     /**
      * Note: create and replace your own key in the Google console.
      * https://console.developers.google.com/apis/dashboard
-     * The key "AIzaSyBkNaAGLEVq0YLQMi-PYEMabFeREadYe1Q" can be ONLY used in this sandbox (no forked).
+     * The key "AIzaSyAbFo3j0y2p5AWljypOJI39FqSjSumWBY4" can be ONLY used in this sandbox (no forked).
      */
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyBhU9khMmAq1H8l4-bNh2k5OQ0xmr71zVw",
+      "https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyAbFo3j0y2p5AWljypOJI39FqSjSumWBY4",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `300px`, width: '590px', maxWidth: `90vw`,padding: '7px', backgroundColor:'rgba(0,0,0,0.6)',}} />,
     mapElement: <div style={{ height: `100%` }} />
